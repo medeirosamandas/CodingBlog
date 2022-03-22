eb047f85f342a4b1fcf2f474137bb3fff9ca10f5
Amanda Medeiros <medeiros.amandas@gmail.com>
2022-03-22 12:42:20 -0300

    Initial commit

---
+++ src/main/java/com/spring/codingblog/CodingblogApplication.java
@@ -0,0 +1,13 @@
+package com.spring.codingblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/codingblog/CodingblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.codingblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
